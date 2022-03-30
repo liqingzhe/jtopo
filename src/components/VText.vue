34a416cf8d0c18d17f5b4fb960aa431f71285586
liqing <liqing_0908@163.com>
2022-03-30 09:08:08 +0800

    添加文本拖拽

---
+++ src/components/VText.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="v-text" @keydown="handleKeydown" @keyup="handleKeyup">
+    <!-- tabindex >= 0 使得双击时聚集该元素 -->
+    <div
+      :contenteditable="canEdit"
+      :class="{ canEdit }"
+      @dblclick="setEdit"
+      :tabindex="node.id"
+      @paste="clearStyle"
+      @mousedown="handleMousedown"
+      @blur="handleBlur"
+      ref="text"
+      v-html="node.propValue"
+      @input="handleInput"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    propValue: {
+      type: String,
+      require: true,
+    },
+    node: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      canEdit: true,
+      ctrlKey: 17,
+      isCtrlDown: false,
+    };
+  },
+  computed: {},
+  methods: {
+    handleInput(e) {
+      this.$emit("input", this.node, e.target.innerHTML);
+    },
+    setFocus() {
+        this.$refs.text.focus()
+    },
+    handleKeydown(e) {
+      if (e.keyCode == this.ctrlKey) {
+        this.isCtrlDown = true;
+      } else if (this.isCtrlDown && this.canEdit) {
+        e.stopPropagation();
+      } else if (e.keyCode == 46) {
+        // deleteKey
+        e.stopPropagation();
+      }
+    },
+
+    handleKeyup(e) {
+      if (e.keyCode == this.ctrlKey) {
+        this.isCtrlDown = false;
+      }
+    },
+
+    handleMousedown(e) {
+      if (this.canEdit) {
+        e.stopPropagation();
+      }
+    },
+
+    clearStyle(e) {
+      e.preventDefault();
+      const clp = e.clipboardData;
+      const text = clp.getData("text/plain") || "";
+      if (text !== "") {
+        document.execCommand("insertText", false, text);
+      }
+      this.$emit("input", this.node, e.target.innerHTML);
+    },
+
+    handleBlur(e) {
+      debugger;
+      this.node.propValue = e.target.innerHTML || "&nbsp;";
+      this.canEdit = false;
+      this.$emit("handleBlur", e.target.innerHTML);
+    },
+
+    setEdit() {
+      this.canEdit = true;
+      // 全选
+      this.selectText(this.$refs.text);
+    },
+
+    selectText(node) {
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(node);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-text {
+  display: table;
+  border: 1px dashed red;
+  padding: 5px 10px;
+  div {
+    display: table-cell;
+    width: 100%;
+    height: 100%;
+    outline: none;
+  }
+
+  .canEdit {
+    cursor: text;
+    height: 100%;
+  }
+}
+
+.preview {
+  user-select: none;
+}
+</style>
--- src/views/Topo/index.vue
+++ src/views/Topo/index.vue
@@ -1,32 +1,40 @@
 <template>
-  <div>
-    <div class="home">
-      <!-- <Toolbar /> -->
-      <main>
-        <!-- 左侧组件列表 -->
-        <section class="left">
-          <ComponentList />
-        </section>
-        <!-- 中间画布 -->
-        <section class="center" id="center">
-          <canvas id="canvas" @drop="handleDrop" @dragover="handleDragOver" />
-          <!-- <div
+  <div class="home">
+    <!-- <Toolbar /> -->
+    <main>
+      <!-- 左侧组件列表 -->
+      <section class="left">
+        <div draggable @dragstart="handleDragText($event)">文本</div>
+        <ComponentList />
+      </section>
+      <!-- 中间画布 -->
+      <section class="center" id="center">
+        <v-text
+          ref="vText"
+          v-if="showInput"
+          :style="setInputPosition()"
+          class="input-text"
+          :node="inputNode"
+          @handleBlur="inputBlur"
+        />
+        <canvas id="canvas" @drop="handleDrop" @dragover="handleDragOver" />
+        <!-- <div
             class="content"
             @drop="handleDrop"
             @dragover="handleDragOver"
             @mousedown="handleMouseDown"
             @mouseup="deselectCurComponent"
           > -->
-          <!-- <Editor /> -->
-          <!-- </div> -->
-        </section>
-      </main>
-    </div>
+        <!-- <Editor /> -->
+        <!-- </div> -->
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
 import ComponentList from "@/components/ComponentList"; // 左侧列表组件
+import VText from "@/components/VText.vue"; // 左侧列表组件
 // import AttrList from '@/components/AttrList' // 右侧属性列表
 // import AnimationList from '@/components/AnimationList' // 右侧动画列表
 // import componentList from '@/custom-component/component-list' // 左侧列表数据
@@ -35,7 +43,7 @@ import { generateUUID } from "@/utils/index.js";
 // import  * as Editor from '@/assets/jtopo/jtopo-editor.js'
 export default {
   name: "Topo",
-  components: { ComponentList },
+  components: { ComponentList, VText },
 
   props: {
     msg: String,
@@ -53,6 +61,14 @@ export default {
       link: null, // 连线
       currentNode: {}, // 当前节点
       currentLink: {}, //当前连线
+      showInput: false,
+      inputNode: {
+        propValue: "文本",
+      },
+      inputTextPosition: {
+        top: 0,
+        left: 0,
+      },
     };
   },
   created() {},
@@ -63,11 +79,15 @@ export default {
     canvas.height = Math.floor(center.offsetHeight - 10);
     this.stage = new JTopo.Stage(canvas); // 创建一个舞台对象
     this.scene = new JTopo.Scene(this.stage); // 创建一个场景对象
+    this.scene.translateX = 0;
+    this.scene.translateY = 0;
     this.lineStart = new JTopo.Node();
     this.lineStart.setSize(1, 1);
     this.lineEnd = new JTopo.Node();
     this.lineEnd.setSize(1, 1);
-    this.link = new JTopo.Link(this.lineStart, this.lineEnd);
+    this.link = new JTopo.FoldLink(this.lineStart, this.lineEnd);
+    this.link.arrowsRadius = 5; //箭头大小
+    // this.stage.mode = "edit";
     this.scene.mouseup((e) => {
       if (e.button == 2) {
         this.scene.remove(this.link);
@@ -75,20 +95,26 @@ export default {
       }
       if (e.target != null && e.target instanceof JTopo.Node) {
         if (this.startNode == null) {
+          // 起始点
           this.startNode = e.target;
           this.scene.add(this.link);
           this.lineStart.setLocation(e.x, e.y);
           this.lineEnd.setLocation(e.x, e.y);
         } else if (this.startNode !== e.target) {
+          // 终点
           this.endNode = e.target;
-          let link = new JTopo.Link(this.startNode, this.endNode);
+          let link = new JTopo.FoldLink(this.startNode, this.endNode);
+          link.arrowsRadius = 5; //箭头大小
           this.scene.add(link);
           this.startNode = null;
           this.scene.remove(this.link);
         } else {
+          // 起点终点设置成了同一个点
           this.startNode = null;
+          this.scene.remove(this.link);
         }
       } else {
+        // 点击非节点区域
         this.startNode = null;
         this.scene.remove(this.link);
       }
@@ -100,19 +126,58 @@ export default {
         e.target === this.startNode ||
         e.target === this.link
       ) {
+        // this.startNode = null;
         this.scene.remove(this.link);
       }
     });
     this.scene.mousemove((e) => {
       this.lineEnd.setLocation(e.x, e.y);
     });
+    // 鼠标移出舞台
+    this.stage.mouseout(() => {
+      // 删掉节点带出来的连线
+      this.startNode = null;
+      this.scene.remove(this.link);
+    });
   },
   methods: {
+    setInputPosition() {
+      return `left:${this.inputTextPosition.left}px;top: ${this.inputTextPosition.top}px;`;
+    },
+    inputBlur(value) {
+      if (value) {
+        this.addTextNode(this.inputTextPosition, value);
+      }
+      this.showInput = false;
+    },
+    addTextNode(location, value) {
+      let textNode = new JTopo.TextNode(value);
+      textNode.setLocation(location.left, location.top);
+      textNode.fontColor = "red";
+      this.scene.add(textNode);
+      textNode.dbclick((e) => {
+        this.scene.remove(textNode);
+        this.showInput = true;
+        this.inputTextPosition.left = e.offsetX;
+        this.inputTextPosition.top = e.offsetY;
+      });
+    },
+    handleDragText(e) {
+      e.dataTransfer.setData("text", JSON.stringify({ type: "Text" }));
+    },
     handleDrop(e) {
       e.preventDefault();
       e.stopPropagation();
       let node = new JTopo.Node();
       let nodeData = JSON.parse(e.dataTransfer.getData("text"));
+      if (nodeData.type == "Text") {
+        this.showInput = true;
+        this.inputTextPosition = {
+          left: e.offsetX,
+          top: e.offsetY,
+        };
+        return;
+      }
       // 节点坐标
       node.setBound(
         e.offsetX,
@@ -128,6 +193,7 @@ export default {
       node.text = nodeData.label;
       node.fontColor = "0,0,0";
       node.nodeImage = nodeData.icon;
+      // node.editAble = nodeData.editAble || true
       this.scene.add(node);
       this.currentNode = node;
     },
@@ -172,6 +238,7 @@ export default {
       height: 100%;
       overflow: auto;
       text-align: center;
+      position: relative;
       .canvas {
         margin: auto;
       }
@@ -181,6 +248,11 @@ export default {
   .placeholder {
     text-align: center;
     color: #333;
+  }
+  .input-text {
+    position: absolute;
+    left: 0;
+    right: 0;
   }
 }
 </style>
